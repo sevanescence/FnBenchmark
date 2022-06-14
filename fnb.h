8c63bea6dc19834a3278a647791796771edae8ff
MakotoMiyamoto <makotonomeru@gmail.com>
2022-06-14 17:06:42 -0500

    code cleanup

--- fnb.h
+++ fnb.h
@@ -1,59 +1,33 @@
 #pragma once
 
-#include <iostream>
 #include <chrono>
 
 template <
-	typename result_t	= std::chrono::milliseconds,
-	typename clock_t	= std::chrono::steady_clock,
-	typename duration_t = std::chrono::milliseconds
+    typename result_t	= std::chrono::milliseconds,
+    typename clock_t	= std::chrono::steady_clock,
+    typename duration_t = std::chrono::milliseconds
 >
-auto since(const std::chrono::time_point<clock_t, duration_t> &start)
+inline auto since(const std::chrono::time_point<clock_t, duration_t> &start)
 {
-	return std::chrono::duration_cast<result_t>(clock_t::now() - start);
+    return std::chrono::duration_cast<result_t>(clock_t::now() - start);
 }
 
 template <
-	typename duration_t = std::chrono::milliseconds,
-	typename R = void(*), typename ... Params
+    typename duration_t = std::chrono::milliseconds,
+    typename R = void(*), typename ... Params
 >
 auto bench(R(*func)(Params...), Params ... params)
 {
-	using namespace std::chrono;
-	steady_clock::time_point start = steady_clock::now();
-	if constexpr (std::is_void_v<decltype(func(params...))>)
-	{
-		func(params...);
-		return since<duration_t>(start);
-	}
-	else if constexpr (!std::is_void_v<decltype(func(params...))>)
-	{
-		auto r = func(params...);
-		return since<duration_t>(start);
-	}
-
+    using namespace std::chrono;
+    steady_clock::time_point start = steady_clock::now();
+    if constexpr (std::is_void_v<decltype(func(params...))>)
+    {
+        func(params...);
+        return since<duration_t>(start);
+    }
+    else if constexpr (!std::is_void_v<decltype(func(params...))>)
+    {
+        auto r = func(params...);
+        return since<duration_t>(start);
+    }
 }
-
-//template <
-//	typename duration_t = std::chrono::milliseconds,
-//	typename R = void(*), typename ... Params
-//>
-//auto bench(R(*func)(Params...), Params ... params) requires (std::is_void_v<decltype(func(params...))>)
-//{
-//	using namespace std::chrono;
-//	steady_clock::time_point start = steady_clock::now();
-//	func(params...);
-//	return since<duration_t>(start).count();
-//}
-//
-//template <
-//	typename duration_t = std::chrono::milliseconds,
-//	typename R = int(*), typename ... Params
-//>
-//auto bench(R(*func)(Params...), Params ... params)
-//{
-//	using namespace std::chrono;
-//	steady_clock::time_point start = steady_clock::now();
-//	auto r = func(params...);
-//	return since<duration_t>(start).count();
-//}
--- test.cpp
+++ test.cpp
@@ -7,43 +7,43 @@
 
 void foo(int b)
 {
-	srand(0);
+    srand(0);
 
-	int a = 0;
-	for (int i = 0; i < 10000000; ++i)
-	{
-		a += rand() % 10;
-	}
+    int a = 0;
+    for (int i = 0; i < 10000000; ++i)
+    {
+        a += rand() % 10;
+    }
 
-	std::cout << "a = " << a << '\n';
+    std::cout << "a = " << a << '\n';
 }
 
 int bar(int a)
 {
-	return a + 1;
+    return a + 1;
 }
 
 int baz()
 {
-	return 2;
+    return 2;
 }
 
 class Class
 {
 public:
-	Class() { std::cout << "Constructor called\n"; }
-	~Class() { std::cout << "Destructor called\n"; }
+    Class() { std::cout << "Constructor called\n"; }
+    ~Class() { std::cout << "Destructor called\n"; }
 };
 
 Class CreateClass()
 {
-	return Class();
+    return Class();
 }
 
 int main()
 {
-	std::cout << "foo(): " << bench(&foo, 3) << "ms\n";
-	std::cout << "bar(): " << bench(&bar, 5) << "ms\n";
-	bench(&baz);
-	std::cout << "CreateClass() " << bench(CreateClass) << "\n";
+    std::cout << "foo(): " << bench(&foo, 3) << "ms\n";
+    std::cout << "bar(): " << bench(&bar, 5) << "ms\n";
+    bench(&baz);
+    std::cout << "CreateClass() " << bench(CreateClass) << "\n";
 }
